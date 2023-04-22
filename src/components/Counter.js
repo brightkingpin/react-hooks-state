a37f8500898379b0c77974c27fbbef62e0d293bb
brightkingpin <luvandabright781@gmail.com>
2023-04-22 22:51:57 +0300

    Done with assignment

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,6 +1,8 @@
+//The useState hook is used to define a piece of state named count and initialize it with a value of 0. The setCount function returned by useState is used to update the value of count.
 import React, { useState } from "react";
 
 function Counter() {
+  //setting the setState function that is returned by the useState.
   const [count, setCount] = useState(0);
 
   function increment() {
